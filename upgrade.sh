d3c553643a3ce427222876afb165a39f610321fa
RedoX <pseudokawaii@gmail.com>
2022-12-31 12:15:55 +0000

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -7,7 +7,7 @@ fi
 curl -O https://beta.rclone.org/rclone-beta-latest-linux-amd64.zip \
     && unzip rclone-beta-latest-linux-amd64.zip \
     && cd rclone-*-linux-amd64 \
-    && cp ./rclone /home/runner/$REPL_SLUG \
+    && cp ./rclone .. \
     && cd .. \
     && chmod +x ./rclone \
     && rm -rf rclone-*-linux-amd64 rclone-beta-latest-linux-amd64.zip
